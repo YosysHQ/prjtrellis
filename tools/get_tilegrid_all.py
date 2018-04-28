9dd05440fe090f6a323123288a65105d3d608997
David Shah <davey1576@gmail.com>
2018-04-28 14:20:15 +0200

    tools: Add get_tilegrid_all.py and related helpers
    Signed-off-by: David Shah <davey1576@gmail.com>

---
+++ tools/get_tilegrid_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+For each family and device, obtain a tilegrid and save it in the database
+"""
+
+import os
+from os import path
+import shutil
+
+import extract_tilegrid
+from common import database, devices, diamond
+
+def main():
+    shutil.rmtree("work_tilegrid", ignore_errors=True)
+    os.mkdir("work_tilegrid")
+    shutil.copy(path.join(database.get_trellis_root(), "minitests", "wire", "wire.v"), "work_tilegrid/wire.v")
+
+    for family in sorted(devices.families.keys()):
+        for device in sorted(devices.families[family]["devices"].keys()):
+            diamond.run(device, "work_tilegrid/wire.v")
+            output_file = path.join(database.get_db_subdir(family, device), "tilegrid.json")
+            extract_tilegrid.main(["extract_tilegrid", "work_tilegrid/wire.tmp/output.test", output_file])
+
+
+if __name__ == "__main__":
+    main()
