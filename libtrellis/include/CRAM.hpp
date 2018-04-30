370a4d9af6cffcedc88159b60d2e700e45265d17
David Shah <davey1576@gmail.com>
2018-04-30 19:41:19 +0200

    libtrellis: Create the CRAM Data Structure
    Signed-off-by: David Shah <davey1576@gmail.com>

--- libtrellis/include/CRAM.hpp
+++ libtrellis/include/CRAM.hpp
@@ -11,7 +11,7 @@ using namespace std;
 class CRAMView {
 public:
     // Access a bit inside the CRAM view by frame and bit offset within the view
-    inline bool &bit(int frame, int bit);
+    char &bit(int frame, int bit);
     // Return the number of frames inside the view
     int frames() const;
     // Return the number of bits inside the view
@@ -19,13 +19,14 @@ public:
 
 private:
     // Private constructor, CRAM::make_view should always be used
-    CRAMView(shared_ptr<vector<vector<bool>>> bits, int frame_offset, int bit_offset, int frame_count, int bit_count);
+    CRAMView(shared_ptr<vector<vector<char>>> data, int frame_offset, int bit_offset, int frame_count, int bit_count);
 
     int frame_offset;
     int bit_offset;
     int frame_count;
     int bit_count;
     friend class CRAM;
+    shared_ptr<vector<vector<char>>> cram_data;
 };
 
 // This represents the chip configuration RAM, and allows views of it to be made (for tile accesses)
@@ -35,7 +36,7 @@ public:
     // Construct empty CRAM given size
     CRAM(int frames, int bits);
     // Access a bit in the CRAM given frame and bit offset
-    inline bool &bit(int frame, int bit);
+    char &bit(int frame, int bit);
     // Return number of frames in CRAM
     int frames() const;
     // Return number of bits per frame in CRAM
@@ -44,7 +45,8 @@ public:
     CRAMView make_view(int frame_offset, int bit_offset, int frame_count, int bit_count);
 private:
     // Using a shared_ptr so views are not invalidated even if the CRAM itself is deleted
-    shared_ptr<vector<vector<bool>>> data;
+    // A vector of type char is used as the optimisations in vector<bool> are not worth the loss of bool& etc
+    shared_ptr<vector<vector<char>>> data;
 };
 
 #endif //LIBTRELLIS_CRAM_HPP
---
+++ libtrellis/src/CRAM.cpp
@@ -0,0 +1,30 @@
+#include "CRAM.hpp"
+#include <cassert>
+
+char &CRAMView::bit(int frame, int bit) {
+    assert(frame < frame_count);
+    assert(bit < bit_count);
+    return cram_data->at(frame_offset + frame).at(bit_offset + bit);
+}
+
+int CRAMView::frames() const { return frame_count; }
+
+int CRAMView::bits() const { return bit_count; }
+
+CRAMView::CRAMView(shared_ptr<vector<vector<char>>> data, int frame_offset, int bit_offset, int frame_count,
+                   int bit_count)
+        : cram_data(data), frame_offset(frame_offset), bit_offset(bit_offset), frame_count(frame_count),
+          bit_count(bit_count) {};
+
+CRAM::CRAM(int frames, int bits) {
+    data = make_shared<vector<vector<char>>>();
+    data->resize(frames, vector<char>(bits));
+}
+
+char &CRAM::bit(int frame, int bit) {
+    return data->at(frame).at(bit);
+}
+
+int CRAM::frames() const { return int(data->size()); }
+
+int CRAM::bits() const { return int(data->at(0).size()); }
