f9e6236097577c63d20e68adfb9a1b1724ab2c1e
David Shah <davey1576@gmail.com>
2018-05-12 19:23:05 +0200

    util: Adding more ispTcl wrapper functions
    Signed-off-by: David Shah <davey1576@gmail.com>

--- util/common/isptcl.py
+++ util/common/isptcl.py
@@ -6,9 +6,9 @@ import database
 import subprocess
 import tempfile
 from os import path
+import re
 
-
-def tcl_run(commands):
+def run(commands):
     """Run a list of Tcl commands, returning the output as a string"""
     dtcl_path = path.join(database.get_trellis_root(), "diamond_tcl.sh")
     workdir = tempfile.mkdtemp()
@@ -31,20 +31,79 @@ def tcl_run(commands):
     output = output.replace(pleasantry, "").strip()
     return output
 
+# All these following commands require a tuple (ncdfile, prffile) containing
+# a specimen design for the target device
 
-def tcl_run_ncd_prf(ncdfile, prffile, commands):
+def run_ncd_prf(desfiles, commands):
     """
     Run a list of Tcl commands after loading given .ncd and .prf files
+
+    desfiles: a tuple (ncdfile, prffile)
+    commands: list of Tcl commands to run
+
+    Returns the output from IspTcl, excluding header and pleasantry
     """
     run_cmds = [
-        "des_read_ncd {}".format(ncdfile),
-        "des_read_prf {}".format(prffile)
+        "des_read_ncd {}".format(desfiles[0]),
+        "des_read_prf {}".format(desfiles[1])
     ] + commands
-    return tcl_run(run_cmds)
+    return run(run_cmds)
+
+
+def get_wires_at_position(desfiles, position):
+    """
+    Use ispTcl to get a list of wires at a given grid position
+
+    desfiles: a tuple (ncdfile, prffile)
+    position: a tuple (row, col)
+
+    Returns a list of tuples
+    (name, type, typeid)
+    """
+    command = ["dev_list_node_at_location -row {} -col {}".format(position[0], position[1])]
+    result = run_ncd_prf(desfiles, command)
+    wires = []
+    for line in result.split('\n'):
+        sline = line.strip()
+        if sline == "":
+            continue
+        splitline = re.split('\s+', sline)
+        assert len(splitline) >= 3
+        wires.append((splitline[2].strip(), splitline[0].strip(), int(splitline[1])))
+    return wires
+
+
+def get_arcs_on_wire(desfiles, wire, drivers_only = False):
+    """
+    Use ispTcl to get a list of arcs sinking or sourcing a given wire
+
+    desfiles: a tuple (ncdfile, prffile)
+    wire: canonical name of the wire
+    drivers_only: only include arcs driving the wire in the output
+
+    Returns a list of arc tuples
+    (source, sink)
+    """
+    command = ["dev_list_arcs -to {} -num 100000".format(wire)]
+    result = run_ncd_prf(desfiles, command)
+    arcs = []
+    for line in result.split('\n'):
+        sline = line.strip()
+        if sline == "":
+            continue
+        splitline = re.split('\s+', sline)
+        assert len(splitline) >= 3
+        if splitline[1].strip() == "-->":
+            arcs.append((splitline[0].strip(), splitline[1].strip()))
+        elif splitline[1].strip() == "<--" and not drivers_only:
+            arcs.append((splitline[1].strip(), splitline[0].strip()))
+        else:
+            assert False, "invalid output from Tcl command `dev_list_arcs`"
+    return arcs
 
 
 def main():
-    print(tcl_run([]))
+    print(run([]))
 
 
 if __name__ == "__main__":
