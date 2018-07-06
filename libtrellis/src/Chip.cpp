6c59b1362fc836e0df85a1f706316e7bb3a422f9
David Shah <davey1576@gmail.com>
2018-07-06 16:04:59 +0200

    libtrellis: Add function to Chip to get routing graph
    Signed-off-by: David Shah <davey1576@gmail.com>

--- libtrellis/src/Chip.cpp
+++ libtrellis/src/Chip.cpp
@@ -2,28 +2,35 @@
 #include "Tile.hpp"
 #include "Database.hpp"
 #include "Util.hpp"
+#include "RoutingGraph.hpp"
+#include "BitDatabase.hpp"
 #include <algorithm>
 
 using namespace std;
 
 namespace Trellis {
 
-Chip::Chip(string name) : Chip(get_chip_info(find_device_by_name(name))) {}
+Chip::Chip(string name) : Chip(get_chip_info(find_device_by_name(name)))
+{}
 
-Chip::Chip(uint32_t idcode) : Chip(get_chip_info(find_device_by_idcode(idcode))) {}
+Chip::Chip(uint32_t idcode) : Chip(get_chip_info(find_device_by_idcode(idcode)))
+{}
 
-Chip::Chip(const Trellis::ChipInfo &info) : info(info), cram(info.num_frames, info.bits_per_frame) {
+Chip::Chip(const Trellis::ChipInfo &info) : info(info), cram(info.num_frames, info.bits_per_frame)
+{
     vector<TileInfo> allTiles = get_device_tilegrid(DeviceLocator{info.family, info.name});
     for (const auto &tile : allTiles) {
         tiles[tile.name] = make_shared<Tile>(tile, *this);
     }
 }
 
-shared_ptr<Tile> Chip::get_tile_by_name(string name) {
+shared_ptr<Tile> Chip::get_tile_by_name(string name)
+{
     return tiles.at(name);
 }
 
-vector<shared_ptr<Tile>> Chip::get_tiles_by_position(int row, int col) {
+vector<shared_ptr<Tile>> Chip::get_tiles_by_position(int row, int col)
+{
     vector<shared_ptr<Tile>> result;
     for (const auto &tile : tiles) {
         if (tile.second->info.get_row_col() == make_pair(row, col))
@@ -32,7 +39,8 @@ vector<shared_ptr<Tile>> Chip::get_tiles_by_position(int row, int col) {
     return result;
 }
 
-vector<shared_ptr<Tile>> Chip::get_tiles_by_type(string type) {
+vector<shared_ptr<Tile>> Chip::get_tiles_by_type(string type)
+{
     vector<shared_ptr<Tile>> result;
     for (const auto &tile : tiles) {
         if (tile.second->info.type == type)
@@ -41,29 +49,33 @@ vector<shared_ptr<Tile>> Chip::get_tiles_by_type(string type) {
     return result;
 }
 
-vector<shared_ptr<Tile>> Chip::get_all_tiles() {
+vector<shared_ptr<Tile>> Chip::get_all_tiles()
+{
     vector<shared_ptr<Tile>> result;
     for (const auto &tile : tiles) {
-         result.push_back(tile.second);
+        result.push_back(tile.second);
     }
     return result;
 }
 
-int Chip::get_max_row() const {
+int Chip::get_max_row() const
+{
     return max_element(tiles.begin(), tiles.end(),
-               [](const decltype(tiles)::value_type &a, const decltype(tiles)::value_type &b) {
-                   return a.second->info.get_row_col().first < b.second->info.get_row_col().first;
-               })->second->info.get_row_col().first;
+                       [](const decltype(tiles)::value_type &a, const decltype(tiles)::value_type &b) {
+                           return a.second->info.get_row_col().first < b.second->info.get_row_col().first;
+                       })->second->info.get_row_col().first;
 }
 
-int Chip::get_max_col() const {
+int Chip::get_max_col() const
+{
     return max_element(tiles.begin(), tiles.end(),
-               [](const decltype(tiles)::value_type &a, const decltype(tiles)::value_type &b) {
-                   return a.second->info.get_row_col().second < b.second->info.get_row_col().second;
-               })->second->info.get_row_col().second;
+                       [](const decltype(tiles)::value_type &a, const decltype(tiles)::value_type &b) {
+                           return a.second->info.get_row_col().second < b.second->info.get_row_col().second;
+                       })->second->info.get_row_col().second;
 }
 
-ChipDelta operator-(const Chip &a, const Chip &b) {
+ChipDelta operator-(const Chip &a, const Chip &b)
+{
     ChipDelta delta;
     for (const auto &tile : a.tiles) {
         CRAMDelta cd = tile.second->cram - b.tiles.at(tile.first)->cram;
@@ -71,6 +83,17 @@ ChipDelta operator-(const Chip &a, const Chip &b) {
             delta[tile.first] = cd;
     }
     return delta;
+}
+
+shared_ptr<RoutingGraph> Chip::get_routing_graph()
+{
+    shared_ptr<RoutingGraph> rg(new RoutingGraph(*this));
+    for (auto tile_entry : tiles) {
+        shared_ptr<Tile> tile = tile_entry.second;
+        shared_ptr<TileBitDatabase> bitdb = get_tile_bitdata(TileLocator{info.family, info.name, tile->info.type});
+        bitdb->add_routing(tile->info, *rg);
+    }
+    return rg;
 }
 
 }
