8fd2d48afb732dfd75c17e411dccff568bef42de
David Shah <davey1576@gmail.com>
2018-07-06 16:55:38 +0200

    libtrellis: Adding a useful working example for the routing graph gen
    Signed-off-by: David Shah <davey1576@gmail.com>

--- libtrellis/examples/graph.py
+++ libtrellis/examples/graph.py
@@ -8,3 +8,14 @@ import sys
 pytrellis.load_database("../../database")
 chip = pytrellis.Chip("LFE5U-25F")
 rg = chip.get_routing_graph()
+tile = rg.tiles[pytrellis.Location(10, 10)]
+for wire in tile.wires:
+    print("Wire {}:".format(rg.to_str(wire.key())))
+    for dh in wire.data().downhill:
+        arc = rg.tiles[dh.loc].arcs[dh.id]
+        print("     --> R{}C{}_{}".format(arc.sink.loc.y, arc.sink.loc.x, rg.to_str(arc.sink.id)))
+    print()
+    for uh in wire.data().uphill:
+        arc = rg.tiles[uh.loc].arcs[uh.id]
+        print("     <-- R{}C{}_{}".format(arc.source.loc.y, arc.source.loc.x, rg.to_str(arc.source.id)))
+    print()
