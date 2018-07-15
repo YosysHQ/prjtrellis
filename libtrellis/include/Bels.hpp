d0911b9c10fc5ae83d144917ef8b7defec43322d
David Shah <davey1576@gmail.com>
2018-07-15 19:15:58 +0200

    libtrellis: Adding Bel definitions for SLICEs and PIOs
    Signed-off-by: David Shah <davey1576@gmail.com>

---
+++ libtrellis/include/Bels.hpp
@@ -0,0 +1,16 @@
+#ifndef BELS_H
+#define BELS_H
+
+#include "Chip.hpp"
+#include "RoutingGraph.hpp"
+
+namespace Trellis {
+namespace Bels {
+
+void add_lc(RoutingGraph &graph, int x, int y, int z);
+void add_pio(RoutingGraph &graph, int x, int y, int z);
+
+}
+}
+
+#endif
