fdb1be4b2c41b2e90a51a40be09daad67ea92681
David Shah <davey1576@gmail.com>
2018-07-28 21:07:37 +0200

    fuzzers: Add all devices to PLL routing fuzzer
    Signed-off-by: David Shah <davey1576@gmail.com>

--- fuzzers/090-pll_routing/fuzzer.py
+++ fuzzers/090-pll_routing/fuzzer.py
@@ -13,6 +13,21 @@ jobs = [
                                  tiles=["MIB_R71C88:PLL0_LR", "MIB_R71C87:PLL1_LR"])),
     ((4, 89), "45K_", FuzzConfig(job="PLLROUTE3", family="ECP5", device="LFE5U-45F", ncl="pllroute.ncl",
                                   tiles=["MIB_R4C90:PLL0_UR", "MIB_R5C90:PLL1_UR"])),
+
+    ((4, 1), "85K_", FuzzConfig(job="PLLROUTE0", family="ECP5", device="LFE5U-85F", ncl="pllroute_85k.ncl",
+                                tiles=["MIB_R4C0:PLL0_UL", "MIB_R5C0:PLL1_UL"])),
+    ((94, 2), "85K_", FuzzConfig(job="PLLROUTE1", family="ECP5", device="LFE5U-85F", ncl="pllroute_85k.ncl",
+                                 tiles=["MIB_R95C2:PLL0_LL", "MIB_R95C3:BANKREF8"])),
+    ((94, 124), "85K_", FuzzConfig(job="PLLROUTE2", family="ECP5", device="LFE5U-85F", ncl="pllroute_85k.ncl",
+                                  tiles=["MIB_R95C124:PLL0_LR", "MIB_R95C123:BANKREF4"])),
+    ((4, 125), "85K_", FuzzConfig(job="PLLROUTE3", family="ECP5", device="LFE5U-85F", ncl="pllroute_85k.ncl",
+                                 tiles=["MIB_R4C126:PLL0_UR", "MIB_R5C126:PLL1_UR"])),
+
+    ((49, 2), "25K_", FuzzConfig(job="PLLROUTE1", family="ECP5", device="LFE5U-25F", ncl="pllroute_25k.ncl",
+                                 tiles=["MIB_R50C2:PLL0_LL", "MIB_R50C3:BANKREF8"])),
+    ((49, 70), "25K_", FuzzConfig(job="PLLROUTE2", family="ECP5", device="LFE5U-25F", ncl="pllroute_25k.ncl",
+                                  tiles=["MIB_R50C70:PLL0_LR", "MIB_R50C69:PLL1_LR"])),
+
 ]
 
 
