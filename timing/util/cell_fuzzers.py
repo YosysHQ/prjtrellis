8fed66c36af5417bfdfaabc3fe89133e8825179c
David Shah <dave@ds0.me>
2018-10-28 13:56:29 +0000

    timing: Infrastructure for cell timing fuzzers
    Signed-off-by: David Shah <dave@ds0.me>

---
+++ timing/util/cell_fuzzers.py
@@ -0,0 +1,41 @@
+"""
+Utilities for fuzzing cell timing
+"""
+from fuzzconfig import FuzzConfig
+import fuzzloops
+import cell_timings
+import timing_dbs
+from os import path
+
+
+def timing_configs(job, design, density="45"):
+    # speedgrade, fuzzconfig
+    return [
+        ("6", FuzzConfig(job + "_6", "ECP5", "LFE5U-{}F".format(density), [], design, "-s 6")),
+        ("7", FuzzConfig(job + "_7", "ECP5", "LFE5U-{}F".format(density), [], design, "-s 7")),
+        ("8", FuzzConfig(job + "_8", "ECP5", "LFE5U-{}F".format(density), [], design, "-s 8")),
+        ("8_5G", FuzzConfig(job + "_8_5G", "ECP5", "LFE5UM5G-{}F".format(density), [], design, "-s 8"))
+    ]
+
+
+def build_and_add(designs, density="45"):
+    jobs = []
+    sdfs = dict()
+    for des in designs:
+        jobs += timing_configs(path.basename(des).replace(".v", ""), des, density)
+    for job in jobs:
+        grade, cfg = job
+        sdfs[grade] = []
+
+    def per_job(job):
+        grade, cfg = job
+        cfg.setup(skip_specimen=True)
+        bitf = cfg.build_design(cfg.ncl, {}, backanno=True)
+        sdf = bitf.replace(".bit", ".sdf")
+        sdfs[grade].append(sdf)
+
+    fuzzloops.parallel_foreach(jobs, per_job)
+    for grade in sdfs.keys():
+        db = timing_dbs.cells_db_path("ECP5", grade)
+        for sdf in sdfs["grade"]:
+            cell_timings.add_sdf_to_database(db, sdf)
