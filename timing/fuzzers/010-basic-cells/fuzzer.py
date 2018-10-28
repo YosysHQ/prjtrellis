96b8db4352ebbad600d71d56903c048adbe17634
David Shah <dave@ds0.me>
2018-10-28 15:42:06 +0000

    timing: Add NES core for fuzzer usage
    Signed-off-by: David Shah <dave@ds0.me>

--- timing/fuzzers/010-basic-cells/fuzzer.py
+++ timing/fuzzers/010-basic-cells/fuzzer.py
@@ -2,7 +2,7 @@ import cell_fuzzers
 
 
 def main():
-    cell_fuzzers.build_and_add(["../../resource/picorv32_large.v", "../../resource/distributed_ram.v"])
+    cell_fuzzers.build_and_add(["../../resource/picorv32_large.v", "../../resource/distributed_ram.v", "../../resource/nescore.v"])
 
 
 if __name__ == "__main__":
