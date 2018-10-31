5c824f09b99c5b02abc33c15810a7862a92d7e6c
David Shah <dave@ds0.me>
2018-10-31 19:53:21 +0000

    libtrellis: Add PLL Bels
    Signed-off-by: David Shah <dave@ds0.me>

--- libtrellis/include/Bels.hpp
+++ libtrellis/include/Bels.hpp
@@ -13,7 +13,7 @@ void add_dcc(RoutingGraph &graph, int x, int y, string side, string z);
 void add_bram(RoutingGraph &graph, int x, int y, int z);
 void add_mult18(RoutingGraph &graph, int x, int y, int z);
 void add_alu54b(RoutingGraph &graph, int x, int y, int z);
-
+void add_pll(RoutingGraph &graph, std::string quad, int x, int y);
 }
 }
 
