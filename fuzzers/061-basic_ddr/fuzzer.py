c79c459b458bdeca1243b08430bfb52186255f6b
David Shah <dave@ds0.me>
2018-11-03 16:32:17 +0000

    fuzzers: Add DATAMUX to DDR fuzzer
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/061-basic_ddr/fuzzer.py
+++ fuzzers/061-basic_ddr/fuzzer.py
@@ -3,76 +3,87 @@ import nonrouting
 import pytrellis
 import fuzzloops
 
+
 jobs = [
     {
         "cfg": FuzzConfig(job="IOLOGICLA", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C0:PICL0", "MIB_R15C0:PICL1", "MIB_R16C0:PICL2"]),
         "side": "L",
         "site": "IOL_L14A",
-        "iol": "A"
+        "iol": "A",
+        "pin": "A2"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICLB", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C0:PICL0", "MIB_R15C0:PICL1", "MIB_R16C0:PICL2"]),
         "side": "L",
         "site": "IOL_L14B",
-        "iol": "B"
+        "iol": "B",
+        "pin": "B1"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICLC", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C0:PICL0", "MIB_R15C0:PICL1", "MIB_R16C0:PICL2"]),
         "side": "L",
         "site": "IOL_L14C",
-        "iol": "C"
+        "iol": "C",
+        "pin": "B2"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICLD", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C0:PICL0", "MIB_R15C0:PICL1", "MIB_R16C0:PICL2"]),
         "side": "L",
         "site": "IOL_L14D",
-        "iol": "D"
+        "iol": "D",
+        "pin": "C2"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICRA", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C72:PICR0", "MIB_R15C72:PICR1", "MIB_R16C72:PICR2"]),
         "side": "R",
         "site": "IOL_R14A",
-        "iol": "A"
+        "iol": "A",
+        "pin": "C20"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICRB", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C72:PICR0", "MIB_R15C72:PICR1", "MIB_R16C72:PICR2"]),
         "side": "R",
         "site": "IOL_R14B",
-        "iol": "B"
+        "iol": "B",
+        "pin": "D19"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICRC", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C72:PICR0", "MIB_R15C72:PICR1", "MIB_R16C72:PICR2"]),
         "side": "R",
         "site": "IOL_R14C",
-        "iol": "C"
+        "iol": "C",
+        "pin": "D20"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICRD", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R14C72:PICR0", "MIB_R15C72:PICR1", "MIB_R16C72:PICR2"]),
         "side": "R",
         "site": "IOL_R14D",
-        "iol": "D"
+        "iol": "D",
+        "pin": "E19"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICTA", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R0C20:PIOT0", "MIB_R0C21:PIOT1", "MIB_R1C20:PICT0", "MIB_R1C21:PICT1"]),
         "side": "T",
         "site": "IOL_T20A",
-        "iol": "A"
+        "iol": "A",
+        "pin": "D9"
     },
     {
         "cfg": FuzzConfig(job="IOLOGICTB", family="ECP5", device="LFE5U-25F", ncl="empty.ncl",
                           tiles=["MIB_R0C20:PIOT0", "MIB_R0C21:PIOT1", "MIB_R1C20:PICT0", "MIB_R1C21:PICT1"]),
         "side": "T",
         "site": "IOL_T20B",
-        "iol": "B"
+        "iol": "B",
+        "pin": "E9"
     },
 ]
 
@@ -82,7 +93,7 @@ def main():
     pytrellis.load_database("../../database")
 
     def per_job(job):
-        def get_substs(ddrtype="", ddren="OFF", gsr="ENABLED", lsrimux="0", clkimux="CLK", lsromux="0", clkomux="CLK", lsrmux="LSR"):
+        def get_substs(ddrtype="", ddren="OFF", gsr="ENABLED", lsrimux="0", clkimux="CLK", lsromux="0", clkomux="CLK", lsrmux="LSR", datamux="PADDO"):
             if ddren == "ON":
                 ddr = "{}:#ON ".format(ddrtype)
             else:
@@ -97,12 +108,13 @@ def main():
                 s = "S"
             else:
                 s = ""
-            return dict(loc=loc, ddr=ddr, gsr=gsr, lsrimux=lsrimux, clkimux=clkimux, lsromux=lsromux, clkomux=clkomux, lsrmux=lsrmux, s=s)
+            return dict(loc=loc, ddr=ddr, gsr=gsr, lsrimux=lsrimux, clkimux=clkimux, lsromux=lsromux, clkomux=clkomux, lsrmux=lsrmux, s=s, pin=pin, datamux=datamux)
 
         cfg = job["cfg"]
         loc = job["site"]
         iol = job["iol"]
         side = job["side"]
+        pin = job["pin"]
 
         cfg.setup()
         empty_bitfile = cfg.build_design(cfg.ncl, {})
@@ -124,6 +136,8 @@ def main():
                                      lambda x: get_substs(clkomux=x), empty_bitfile, False)
         nonrouting.fuzz_enum_setting(cfg, "IOLOGIC{}.LSRMUX".format(iol), ["LSR", "INV"],
                                      lambda x: get_substs(lsrmux=x), empty_bitfile, False)
+        nonrouting.fuzz_enum_setting(cfg, "PIO{}.DATAMUX_ODDR".format(iol), ["PADDO", "IOLDO"],
+                                     lambda x: get_substs(datamux=x), empty_bitfile, False)
     fuzzloops.parallel_foreach(jobs, per_job)
 
 
