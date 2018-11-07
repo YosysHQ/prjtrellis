be32d9903f8d8428071c8f33823dcf86d86374cf
David Shah <dave@ds0.me>
2018-11-07 20:45:03 +0000

    extra DCU bels wip
    Signed-off-by: David Shah <dave@ds0.me>

--- libtrellis/include/Bels.hpp
+++ libtrellis/include/Bels.hpp
@@ -15,6 +15,8 @@ void add_mult18(RoutingGraph &graph, int x, int y, int z);
 void add_alu54b(RoutingGraph &graph, int x, int y, int z);
 void add_pll(RoutingGraph &graph, std::string quad, int x, int y);
 void add_dcu(RoutingGraph &graph, int x, int y);
+void add_extref(RoutingGraph &graph, int x, int y);
+void add_pcsclkdiv(RoutingGraph &graph, int x, int y, int z);
 }
 }
 
