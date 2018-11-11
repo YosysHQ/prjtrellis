58300e2b77b0dda529ef39ac40332bc64ff87666
William D. Jones <thor0505@comcast.net>
2018-11-11 12:20:32 -0500

    fuzzer: Gate MachXO2 OSCH fuzzing with command line arg to avoid recalculating frequency params.
    Signed-off-by: William D. Jones <thor0505@comcast.net>

--- fuzzers/machxo2/102-oscg/fuzzer.py
+++ fuzzers/machxo2/102-oscg/fuzzer.py
@@ -1,3 +1,5 @@
+import sys
+
 from fuzzconfig import FuzzConfig
 import nonrouting
 import pytrellis
@@ -7,7 +9,9 @@ import interconnect
 cfg = FuzzConfig(job="OSCH", family="MachXO2", device="LCMXO2-1200HC", ncl="empty.ncl",
                                           tiles=["PT8:PIC_T_DUMMY_OSC",
                                                 "PT4:CFG0", "PT5:CFG1",
-                                                "PT6:CFG2", "PT7:CFG3"])
+                                                "PT6:CFG2", "PT7:CFG3",
+                                                "CIB_R1C4:CIB_CFG0",
+                                                "CIB_R1C5:CIB_CFG1"])
 
 
 def get_substs(mode="OSCH", nom_freq="2.08"):
@@ -34,25 +38,28 @@ def main():
     empty_bitfile = cfg.build_design(cfg.ncl, {})
     cfg.ncl = "osc.ncl"
 
-    nonrouting.fuzz_enum_setting(cfg, "OSCH.MODE", ["NONE", "OSCH"],
-                                 lambda x: get_substs(mode=x), empty_bitfile)
-    nonrouting.fuzz_enum_setting(cfg, "OSCH.NOM_FREQ",
-        ["{}".format(i) for i in [
-            2.08, 2.15, 2.22, 2.29, 2.38, 2.46, 2.56, 2.66, 2.77, 2.89,
-            3.02, 3.17, 3.33, 3.50, 3.69, 3.91, 4.16, 4.29, 4.43, 4.59,
-            4.75, 4.93, 5.12, 5.32, 5.54, 5.78, 6.05, 6.33, 6.65, 7.00,
-            7.39, 7.82, 8.31, 8.58, 8.87, 9.17, 9.50, 9.85, 10.23, 10.64,
-            11.08, 11.57, 12.09, 12.67, 13.30, 14.00, 14.78, 15.65, 15.65, 16.63,
-            17.73, 19.00, 20.46, 22.17, 24.18, 26.60, 29.56, 33.25, 38.00, 44.33,
-            53.20, 66.50, 88.67, 133.00
-        ]], lambda x: get_substs(nom_freq=x), empty_bitfile)
+    # Takes a long time, so permit opt-out.
+    if "-s" not in sys.argv:
+        nonrouting.fuzz_enum_setting(cfg, "OSCH.MODE", ["NONE", "OSCH"],
+                                     lambda x: get_substs(mode=x), empty_bitfile)
+        nonrouting.fuzz_enum_setting(cfg, "OSCH.NOM_FREQ",
+            ["{}".format(i) for i in [
+                2.08, 2.15, 2.22, 2.29, 2.38, 2.46, 2.56, 2.66, 2.77, 2.89,
+                3.02, 3.17, 3.33, 3.50, 3.69, 3.91, 4.16, 4.29, 4.43, 4.59,
+                4.75, 4.93, 5.12, 5.32, 5.54, 5.78, 6.05, 6.33, 6.65, 7.00,
+                7.39, 7.82, 8.31, 8.58, 8.87, 9.17, 9.50, 9.85, 10.23, 10.64,
+                11.08, 11.57, 12.09, 12.67, 13.30, 14.00, 14.78, 15.65, 15.65, 16.63,
+                17.73, 19.00, 20.46, 22.17, 24.18, 26.60, 29.56, 33.25, 38.00, 44.33,
+                53.20, 66.50, 88.67, 133.00
+            ]], lambda x: get_substs(nom_freq=x), empty_bitfile)
 
-    # cfg.ncl = "osc_routing.ncl"
-    # interconnect.fuzz_interconnect_with_netnames(
-    #     cfg,
-    #     ["R70C4_JOSC_OSC"],
-    #     bidir=True
-    # )
+    cfg.ncl = "osc_routing.ncl"
+    interconnect.fuzz_interconnect_with_netnames(
+        cfg,
+        ["R1C4_JOSC_OSC"],
+        bidir=True,
+        netdir_override={"R1C4_JOSC_OSC" : "driver"}
+    )
 
 if __name__ == "__main__":
     main()
