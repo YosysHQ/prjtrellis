78442531597080f269a80f8e884a4ace652669cd
David Shah <dave@ds0.me>
2018-11-13 15:33:53 +0000

    timing: Add EBR related pips
    Signed-off-by: David Shah <dave@ds0.me>

--- timing/util/pip_classes.py
+++ timing/util/pip_classes.py
@@ -1,7 +1,7 @@
 import re
 import tiles
-lc_input_re = re.compile(r'[ABCDM]\d')
-lc_output_re = re.compile(r'[FQ]\d')
+lc_input_re = re.compile(r'(J?[ABCDM]|CLK|LSR|CE)\d')
+lc_output_re = re.compile(r'J?[FQ]\d')
 
 def get_span(wire):
     if (wire.startswith("H") or wire.startswith("V")) and wire[1:3].isdigit():
@@ -49,22 +49,28 @@ def get_pip_class(source, sink):
         return None
     if source_base.endswith("_SLICE") or source_base.startswith("MUX") or sink_base.endswith("_SLICE"):
         return "slice_internal"
+    if sink_base.endswith("_EBR") or source_base.endswith("_EBR"):
+        return "ebr_internal"
     if lc_input_re.match(sink_base):
         if lc_output_re.match(source_base):
-            return "slice_{}_to_slice_{}".format(source_base[0].lower(), sink_base[0].lower())
+            return "{}_to_{}".format(source_base[:-1].lower(), sink_base[:-1].lower())
         elif get_span(source_base) is not None:
-            return get_span(source_base)  + "_to_" + sink_base[0].lower() + format_rel(source_loc, sink_loc)
+            return get_span(source_base)  + "_to_" + sink_base[:-1].lower() + format_rel(source_loc, sink_loc)
         else:
             assert False, (source, sink)
     elif get_span(sink_base) is not None:
         if lc_output_re.match(source_base):
-            return  "slice_" + source_base[0].lower() + "_to_" + get_span(sink_base) + format_rel(source_loc, sink_loc)
+            return source_base[:-1].lower() + "_to_" + get_span(sink_base) + format_rel(source_loc, sink_loc)
         elif get_span(source_base) is not None:
             return  get_span(source_base) + "_to_" + get_span(sink_base) + format_rel(source_loc, sink_loc)
         elif source_base.startswith("HPBX"):
             return "global_to_" + get_span(sink_base)
         else:
             assert False, (source, sink)
+    elif source_base.startswith("CLK") and sink_base.startswith("MUXCLK"):
+        return "clk_to_muxclk"
+    elif source_base.startswith("LSR") and sink_base.startswith("MUXLSR"):
+        return "lsr_to_muxlsr"
     else:
         return None
 
