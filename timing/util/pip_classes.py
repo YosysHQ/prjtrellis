9cd61e184f3a4a37cb489dc362d530539e8650c7
David Shah <dave@ds0.me>
2018-11-15 16:56:32 +0000

    timing: Fix pip classes for db gen
    Signed-off-by: David Shah <dave@ds0.me>

--- timing/util/pip_classes.py
+++ timing/util/pip_classes.py
@@ -52,20 +52,28 @@ def get_pip_class(source, sink):
         return "slice_internal"
     if sink_base.endswith("_EBR") or source_base.endswith("_EBR"):
         return "ebr_internal"
+    if "TEST" in sink_base or "TEST" in source_base:
+        return None
+    if "ALU" in sink_base or "ALU" in source_base or "MULT" in sink_base or "MULT" in source_base or "PRADD" in sink_base:
+        return "dsp_internal"
     if lc_input_re.match(sink_base):
         if lc_output_re.match(source_base):
             return "{}_to_{}".format(source_base[:-1].lower(), sink_base[:-1].lower())
         elif get_span(source_base) is not None:
             return get_span(source_base)  + "_to_" + sink_base[:-1].lower() + format_rel(source_loc, sink_loc)
+        elif "HPBX" in source_base:
+            return "global_to_" + sink_base[:-1].lower()
         else:
-            assert False, (source, sink)
+            return None
     elif get_span(sink_base) is not None:
         if lc_output_re.match(source_base):
             return source_base[:-1].lower() + "_to_" + get_span(sink_base) + format_rel(source_loc, sink_loc)
         elif get_span(source_base) is not None:
             return  get_span(source_base) + "_to_" + get_span(sink_base) + format_rel(source_loc, sink_loc)
-        elif source_base.startswith("HPBX"):
+        elif "HPBX" in source_base:
             return "global_to_" + get_span(sink_base)
+        elif "BOUNCE" in source_base:
+            return None
         else:
             assert False, (source, sink)
     elif source_base.startswith("LSR") and sink_base.startswith("MUXLSR"):
