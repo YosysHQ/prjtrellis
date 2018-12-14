edcc1c04f14e3d661788fc39c5b4b0e2ee7ebd5f
David Shah <dave@ds0.me>
2018-12-14 14:52:36 +0000

    minitests: Fix ODDR minitest
    Signed-off-by: David Shah <dave@ds0.me>

--- minitests/iologic/oddr.v
+++ minitests/iologic/oddr.v
@@ -1,4 +1,4 @@
-module iddr(input D, SCLK, RST, output Q0, Q1);
-IDDRX1F iddr_i(.D(D), .SCLK(SCLK), .RST(RST), .Q0(Q0), .Q1(Q1));
+module oddr(output Q, input SCLK, RST, D0, D1);
+ODDRX1F oddr_i(.Q(Q), .SCLK(SCLK), .RST(RST), .D0(D0), .D1(D1));
 endmodule
 
