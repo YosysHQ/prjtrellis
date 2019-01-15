5e8ca97a00c8511ead0522d7eac1e27609a781e7
Michael Nolan <mtnolan2640@gmail.com>
2019-01-14 22:04:41 -0500

    Add LVDS I/Os
    Signed-off-by: Michael Nolan <mtnolan2640@gmail.com>

---
+++ sim/ILVDS.v
@@ -0,0 +1,5 @@
+module ILVDS(input A, AN, output Z);
+    assign Z = A; //yeah, they really did this, no checking of AN
+endmodule // ILVDS
+
+    
---
+++ sim/OLVDS.v
@@ -0,0 +1,4 @@
+module OLVDS(input A, output Z, ZN);
+    assign Z = A;
+    assign ZN = ~A;
+endmodule // OLVDS
