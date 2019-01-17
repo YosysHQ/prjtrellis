7679e2060731fc4b8d41df787a80c154408bcf1a
David Shah <dave@ds0.me>
2019-01-17 11:31:27 +0000

    Fix 25k EBR
    Signed-off-by: David Shah <dave@ds0.me>

--- fuzzers/ECP5/047-copy_ebr_config/fuzzer.py
+++ fuzzers/ECP5/047-copy_ebr_config/fuzzer.py
@@ -8,8 +8,8 @@ def main():
         "MIB_EBR8": ["EBR_SPINE_UL0", "EBR_SPINE_UL1", "EBR_SPINE_UL2", "EBR_CMUX_UL",
                      "EBR_SPINE_UR0", "EBR_SPINE_UR1", "EBR_SPINE_UR2",
                      "EBR_SPINE_LL0", "EBR_SPINE_LL1", "EBR_SPINE_LL2", "EBR_CMUX_LL",
-                     "EBR_SPINE_LR0", "EBR_SPINE_LR1", "EBR_SPINE_LR2"],
-        "MIB_EBR0": ["EBR_CMUX_UR", "EBR_CMUX_LR"],
+                     "EBR_SPINE_LR0", "EBR_SPINE_LR1", "EBR_SPINE_LR2", "EBR_CMUX_LL_25K"],
+        "MIB_EBR0": ["EBR_CMUX_UR", "EBR_CMUX_LR", "EBR_CMUX_LR_25K"],
     }
     for src, dest_tiles in sorted(copy_rules.items()):
         for dest in dest_tiles:
