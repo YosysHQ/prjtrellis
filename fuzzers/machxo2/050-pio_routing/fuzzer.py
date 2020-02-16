772901f442a74195016cb23bb97d6cd6758f4253
William D. Jones <thor0505@comcast.net>
2020-02-16 05:42:56 -0500

    Refine PIO Routing fuzzer similiar to Center MUX fuzzer.

--- fuzzers/machxo2/050-pio_routing/fuzzer.py
+++ fuzzers/machxo2/050-pio_routing/fuzzer.py
@@ -9,72 +9,7 @@ import re
 import argparse
 
 import isptcl
-
-# Hint: F/Q are sinks for "driver"s, A-D are sources for "sinks".
-# Bottom Fuzzing
-b_overrides = dict()
-for (n, r, d) in [(["R12C11_JRXDA{}_BIOLOGIC"], range(0,8), "driver"),
-                  (["R12C11_JDI{}",
-                    "R12C11_JIN{}_IOLOGIC"], nets.char_range("A","E"), "driver"),
-                  (["R12C11_JPADDO{}",
-                    "R12C11_JPADDT{}"], nets.char_range("A","E"), "driver"),
-                  (["R12C11_JRXD{}A_BIOLOGIC",
-                    "R12C11_JRXD{}C_BSIOLOGIC"], range(0,4), "driver"),
-                  (["R12C11_JDEL{}A_BIOLOGIC",
-                    "R12C11_JDEL{}C_BSIOLOGIC"], range(0,5), "sink"),
-                  (["R12C11_JI{}A_BIOLOGIC",
-                    "R12C11_JI{}B_IOLOGIC",
-                    "R12C11_JI{}C_BSIOLOGIC",
-                    "R12C11_JI{}D_IOLOGIC"], ["N", "P"], "sink"),
-                  (["R12C11_JOPOS{}",
-                    "R12C11_JONEG{}",
-                    "R12C11_JTS{}",
-                    "R12C11_JCLK{}",
-                    "R12C11_JLSR{}",
-                    "R12C11_JCE{}"], ["A_BIOLOGIC",
-                                     "B_IOLOGIC",
-                                     "C_BSIOLOGIC",
-                                     "D_IOLOGIC"], "sink"),
-                    (["R12C11_JSLIP{}"], ["A_BIOLOGIC",
-                                         "C_BSIOLOGIC"], "sink")
-              ]:
-    for p in nets.net_product(n, r):
-        b_overrides[p] = d
-
-b_cib_overrides = defaultdict(lambda : str("ignore"))
-for (n, r, d) in [(["R11C11_JA{}",
-                    "R11C11_JB{}",
-                    "R11C11_JC{}",
-                    "R11C11_JCLK{}",
-                    "R11C11_LSR{}",
-                    "R11C11_JCE{}"], range(0,4), "sink"),
-                  (["R11C11_JQ{}"], range(0,4), "driver"),
-                  (["R11C11_JF{}"], range(0,8), "driver")
-              ]:
-    for p in nets.net_product(n, r):
-        b_cib_overrides[p] = d
-
-def nn_filterb(net, netnames):
-    return not nets.is_cib(net)
-
-
-# Left and Right are done from CIB's POV because
-# there are no tiles dedicated strictly to I/O connections.
-# Ignore loopback/CIBTEST nets.
-l_overrides = defaultdict(lambda : str("ignore"))
-
-# grep "R10C1_J.*" r10c1.txt | grep -v "CIBTEST" | sort | less
-for (n, r, d) in [(["R10C1_JA{}",
-                    "R10C1_JB{}",
-                    "R10C1_JC{}",
-                    "R10C1_JCLK{}",
-                    "R10C1_LSR{}",
-                    "R10C1_JCE{}"], range(0,4), "driver"),
-                  (["R10C1_JQ{}"], range(0,4), "sink"),
-                  (["R10C1_JF{}"], range(0,8), "sink")
-              ]:
-    for p in nets.net_product(n, r):
-        l_overrides[p] = d
+import mk_nets
 
 span1_re = re.compile(r'R\d+C\d+_[VH]01[NESWTLBR]\d{4}')
 def nn_filterl(net, netnames):
@@ -82,6 +17,8 @@ def nn_filterl(net, netnames):
     left out by Tcl"""
     return net in netnames or nets.is_global(net) or span1_re.match(net)
 
+def nn_filterb(net, netnames):
+    return net in netnames or nets.is_global(net) or span1_re.match(net)
 
 jobs = [
         {
@@ -89,21 +26,21 @@ jobs = [
            "cfg" : FuzzConfig(job="PIOROUTEB", family="MachXO2", device="LCMXO2-1200HC", ncl="pioroute.ncl",
                                   tiles=["PB11:PIC_B0"]),
            "nn_filter" : nn_filterb,
-           "netnames_override" : b_overrides,
+           "override_id" : "b",
         },
         {
            "pos" : [(11, 11)],
            "cfg" : FuzzConfig(job="PIOROUTEB_CIB", family="MachXO2", device="LCMXO2-1200HC", ncl="pioroute.ncl",
                                   tiles=["CIB_R11C11:CIB_PIC_B0"]),
            "nn_filter" : nn_filterb,
-           "netnames_override" : b_cib_overrides,
+           "override_id" : "b_cib",
         },
         {
            "pos" : [(10, 1)],
            "cfg" : FuzzConfig(job="PIOROUTEL", family="MachXO2", device="LCMXO2-1200HC", ncl="pioroute.ncl",
                                   tiles=["PL10:PIC_L0"]),
            "nn_filter" : nn_filterl,
-           "netnames_override" : l_overrides,
+           "override_id" : "l",
         },
 
         # Probably the same thing as PIC_L0 plus some additional fixed connections?
@@ -112,7 +49,7 @@ jobs = [
            "cfg" : FuzzConfig(job="PIOROUTEL", family="MachXO2", device="LCMXO2-1200HC", ncl="pioroute.ncl",
                                   tiles=["PL11:LLC0"]),
            "nn_filter" : nn_filterl,
-           "netnames_override" : l_overrides,
+           "override_id" : "llc",
         },
 
         {
@@ -120,7 +57,7 @@ jobs = [
             "cfg" : FuzzConfig(job="PIOROUTER", family="MachXO2", device="LCMXO2-1200HC", ncl="pioroute.ncl",
                                    tiles=["PR10:PIC_R0"]),
             "nn_filter" : nn_filterl,
-            "netnames_override" : l_overrides,
+            "override_id" : "r",
         },
 ]
 
@@ -130,19 +67,13 @@ def main(args):
         cfg = job["cfg"]
         cfg.setup()
 
-        interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=["R11C11_JA0"],
-                                                     netname_filter_union=False,
-                                                     netdir_override=defaultdict(lambda : str("ignore")),
-                                                     bias=1)
-
-        # FIXME: This excludes a significant number of wires for some reason...
-        # for pos in job["pos"]:
-        #     interconnect.fuzz_interconnect(config=cfg, location=pos,
-        #                                    netname_predicate=job["nn_filter"],
-        #                                    netdir_override=job["netnames_override"],
-        #                                    netname_filter_union=False,
-        #                                    enable_span1_fix=True,
-        #                                    bias=1)
+    for pos in job["pos"]:
+        interconnect.fuzz_interconnect(config=cfg, location=pos,
+                                       netname_predicate=job["nn_filter"],
+                                       netdir_override=mk_nets.overrides[job["override_id"]],
+                                       netname_filter_union=False,
+                                       enable_span1_fix=True,
+                                       bias=1)
 
 
 if __name__ == "__main__":
---
+++ fuzzers/machxo2/050-pio_routing/mk_nets.py
@@ -0,0 +1,95 @@
+from nets import net_product, char_range
+from itertools import product, starmap
+from collections import defaultdict
+
+# A, B, C, D, F, M, Q- 0-7
+# CE, CLK, LSR- 0-3
+# Useful helpers to flatten nested iterators.
+def char_product(chars, ids):
+    return starmap(lambda l, n: "".join([l, n]), product(chars, ids))
+
+# Hint: F/Q are sinks for "driver"s, A-D are sources for "sinks".
+templates_override = {
+    "b": [
+        (["R12C11_JRXDA{}_BIOLOGIC"], range(0,8), "driver"),
+        (["R12C11_JDI{}",
+          "R12C11_JIN{}_IOLOGIC"], char_range("A","E"), "driver"),
+        (["R12C11_JPADDO{}",
+          "R12C11_JPADDT{}"], char_range("A","E"), "driver"),
+        (["R12C11_JRXD{}A_BIOLOGIC",
+          "R12C11_JRXD{}C_BSIOLOGIC"], range(0,4), "driver"),
+        (["R12C11_JDEL{}A_BIOLOGIC",
+          "R12C11_JDEL{}C_BSIOLOGIC"], range(0,5), "sink"),
+        (["R12C11_JI{}A_BIOLOGIC",
+          "R12C11_JI{}B_IOLOGIC",
+          "R12C11_JI{}C_BSIOLOGIC",
+          "R12C11_JI{}D_IOLOGIC"], ["N", "P"], "sink"),
+        (["R12C11_JOPOS{}",
+          "R12C11_JONEG{}",
+          "R12C11_JTS{}",
+          "R12C11_JCLK{}",
+          "R12C11_JLSR{}",
+          "R12C11_JCE{}"], ["A_BIOLOGIC", "B_IOLOGIC", "C_BSIOLOGIC",
+                            "D_IOLOGIC"], "sink"),
+          (["R12C11_JSLIP{}"], ["A_BIOLOGIC",
+                               "C_BSIOLOGIC"], "sink")
+    ],
+
+    # Manually verified to be safe to ignore. They are either I/O connections
+    # we fuzz in "b" (BIOLOGIC, etc), CIBTEST, or extraneous connections
+    # that would imply bidirectional nets
+    # (R11C11_V02N0001 --- R11C11_V02S0000, etc). TODO: Look into bidir nets?
+    "b_cib": [
+        # (["R11C11_J{}_CIBTEST"], char_product(["A", "B", "C", "D", "F", "M",
+        #                                        "Q", "OFX"],
+        #                                       char_range("0", "8")), "ignore"),
+        # (["R11C11_J{}_CIBTEST"], char_product(["CE", "CLK", "LSR"],
+        #                                       char_range("0", "4")), "ignore"),
+        # (["R11C11_J{}"], char_product(["A", "B", "C", "D", "M"],
+        #                               char_range("0", "8")), "sink"),
+        # (["R11C11_J{}"], char_product(["CE", "CLK", "LSR"],
+        #                               char_range("0", "4")), "sink"),
+        # (["R11C11_J{}"], char_product(["F", "Q", "OFX"], char_range("0", "8")),
+        #                               "source"),
+    ],
+
+    # Left and Right are done from CIB's POV because
+    # there are no tiles dedicated strictly to I/O connections.
+    # Ignore loopback/CIBTEST nets.
+    "l": [
+        (["R10C1_JA{}",
+          "R10C1_JB{}",
+          "R10C1_JC{}",
+          "R10C1_JCLK{}",
+          "R10C1_LSR{}",
+          "R10C1_JCE{}"], range(0,4), "driver"),
+        (["R10C1_JQ{}"], range(0,4), "sink"),
+        (["R10C1_JF{}"], range(0,8), "sink")
+    ]
+}
+
+overrides = {k: defaultdict(lambda : str("ignore")) for k in templates_override.keys()}
+# overrides = {k: {} for k in templates_override.keys()}
+
+for (var, temp) in templates_override.items():
+    for (n, r, d) in temp:
+        for p in net_product(n, r):
+            overrides[var][p] = d
+
+
+def main():
+    for k, v in overrides.items():
+        print("{}: {}".format(k, v))
+        print("")
+
+    sum = 0
+    for k in overrides.keys():
+        s = len(overrides[k])
+        print("total {}: {}".format(k, s))
+        sum = sum + s
+
+    print("grand total: {}".format(sum))
+
+
+if __name__ == "__main__":
+    main()
