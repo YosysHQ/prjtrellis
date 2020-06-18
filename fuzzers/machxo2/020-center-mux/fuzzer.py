43cf6e5fb42b89d371a74c10d0078570a6603488
William D. Jones <thor0505@comcast.net>
2020-06-18 17:13:19 -0400

    Refactor 020-center-mux (not run yet).

--- fuzzers/machxo2/020-center-mux/fuzzer.py
+++ fuzzers/machxo2/020-center-mux/fuzzer.py
@@ -5,50 +5,47 @@ import interconnect
 import nets
 import pytrellis
 import re
-import mk_nets
+import argparse
+from mk_nets import *
 
+# The relevant tiles were inferred from the center_mux experiment.
 jobs = [
-        # Global mux connections. The relevant tiles were inferred from the
-        # center_mux experiment.
-        ("fixed", FuzzConfig(job="GLOBAL_MUX", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
-                  tiles=["CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
-                         "CENTER6:CENTER_EBR_CIB", "CENTER5:CENTER5"])),
-
-        # ("ignored", FuzzConfig(job="GLOBAL_MUX", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
-        #           tiles=["CENTER6:CENTER_EBR_CIB", "CENTER5:CENTER5", "CENTER4:CENTER4"])),
-
-        # Fixed connections within the global mux (as well as
-        # direction select).
-        ("muxed", FuzzConfig(job="GLOBAL_FIXED", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
-                  tiles=["CENTER6:CENTER_EBR_CIB"])),
+        {
+            "netnames" : eclk_out + eclk_cib + eclk_div,
+            "cfg" : FuzzConfig(job="GLOBAL_FIXED", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
+                      tiles=["CENTER6:CENTER_EBR_CIB", "CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
+                             "CENTER5:CENTER5", "CENTER4:CENTER4"]),
+            "prefix" : "1200_"
+        }
 ]
 
 
-def main():
+def main(args):
     pytrellis.load_database("../../../database")
 
-    for job in jobs:
-        net_id, cfg = job
+    for job in [jobs[i] for i in args.ids]:
+        cfg = job["cfg"]
         cfg.setup()
-
-        netnames = mk_nets.nets[net_id]
-        interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=netnames,
+        interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=job["netnames"],
                                                      netname_filter_union=False,
                                                      netdir_override=defaultdict(lambda : str("sink")),
-                                                     bias=1)
+                                                     nonlocal_prefix=job["prefix"])
 
     # TODO: R6C13_JA0 --> R6C13_JCE0_DCC. But TCL also claims
     # R6C13_CLKI0_DCC --> R6C13_CLKO0_DCC (pseudo = 1). Contradiction?
     # From talking to Dave: No it's not a contradiction. A
     # config bit controls whether JCE0 has any effect.
+    # Might be fuzzed with something like:
     # interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=["R6C13_CLKI0_DCC", "R6C13_CLKO0_DCC", "R6C13_JCE0_DCC"],
     #                                              netname_filter_union=False,
     #                                              netdir_override = {
     #                                                 "R6C13_JCE0_DCC" : "sink",
-    #                                              },
-    #                                              full_mux_style=True,
-    #                                              bias=1)
+    #                                              })
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Center Mux Routing Fuzzer.")
+    parser.add_argument(dest="ids", metavar="N", type=int, nargs="*",
+                    default=range(0, len(jobs)), help="Job (indices) to run.")
+    args = parser.parse_args()
+    main(args)
