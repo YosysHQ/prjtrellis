af8ceb6710c8722a4a54dd7c28dfc787ca5a6026
William D. Jones <thor0505@comcast.net>
2020-07-10 00:10:23 -0400

    Improve filters for center_mux fuzzer.

--- fuzzers/machxo2/020-center-mux/fuzzer.py
+++ fuzzers/machxo2/020-center-mux/fuzzer.py
@@ -8,6 +8,15 @@ import re
 import argparse
 from mk_nets import *
 
+def fc_filter_all(a, n):
+    return True
+
+# TCL claims these are fixed connections; we should treat them as
+# inputs/outputs to/from DCC/DCM BELs.
+def fc_filter_dcc(a, n):
+    return bool(not (nets.machxo2.dcc_sig_re.match(a[0]) and nets.machxo2.dcc_sig_re.match(a[1])) and
+                not (nets.machxo2.dcm_sig_re.match(a[0]) and nets.machxo2.dcm_sig_re.match(a[1])))
+
 # The relevant tiles were inferred from the center_mux experiment.
 # Asterisk means "the fuzzer generated connects with non-local prefixes".
 jobs = [
@@ -19,7 +28,8 @@ jobs = [
                              "CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
                              "CENTER5:CENTER5", "CENTER4:CENTER4"]),
             "prefix" : "1200_",
-            "overrides" : defaultdict(lambda : str("sink"))
+            "overrides" : defaultdict(lambda : str("sink")),
+            "fc_filter" : fc_filter_all
         },
 
         # *
@@ -33,7 +43,8 @@ jobs = [
             "overrides" : { "R6C13_JECSOUT0_ECLKBRIDGECS" : "driver",
                             "R6C13_JECSOUT1_ECLKBRIDGECS" : "driver",
                             "R6C13_JSEL0_ECLKBRIDGECS" : "sink",
-                            "R6C13_JSEL1_ECLKBRIDGECS" : "sink" }
+                            "R6C13_JSEL1_ECLKBRIDGECS" : "sink" },
+            "fc_filter" : fc_filter_all
         },
 
         {
@@ -43,17 +54,19 @@ jobs = [
                              "CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
                              "CENTER5:CENTER5", "CENTER4:CENTER4"]),
             "prefix" : "1200_",
-            "overrides" : defaultdict(lambda : str("sink"))
+            "overrides" : defaultdict(lambda : str("sink")),
+            "fc_filter" : fc_filter_dcc
         },
 
         {
             "netnames" : hfsn_cib + hfsn_out + global_cib + global_out + pll + clock_pin,
-            "cfg" : FuzzConfig(job="GLOBAL_DCM_DCC", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
+            "cfg" : FuzzConfig(job="GLOBAL_HFSN", family="MachXO2", device="LCMXO2-1200HC", ncl="center-mux.ncl",
                       tiles=["CENTER6:CENTER_EBR_CIB", "CENTER_EBR14:CENTER_EBR",
                              "CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
                              "CENTER5:CENTER5", "CENTER4:CENTER4"]),
             "prefix" : "1200_",
-            "overrides" : defaultdict(lambda : str("sink"))
+            "overrides" : defaultdict(lambda : str("sink")),
+            "fc_filter" : fc_filter_all
         },
 ]
 
@@ -67,7 +80,8 @@ def main(args):
         interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=job["netnames"],
                                                      netname_filter_union=False,
                                                      netdir_override=job["overrides"],
-                                                     nonlocal_prefix=job["prefix"])
+                                                     nonlocal_prefix=job["prefix"],
+                                                     fc_predicate=job["fc_filter"])
 
     # TODO: R6C13_JA0 --> R6C13_JCE0_DCC. But TCL also claims
     # R6C13_CLKI0_DCC --> R6C13_CLKO0_DCC (pseudo = 1). Contradiction?
