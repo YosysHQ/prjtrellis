d2659ed49a98cd513041285e018bd6f71b658dad
William D. Jones <thor0505@comcast.net>
2020-07-14 02:39:32 -0400

    Add more conjectured tiles with identical layout to 035-copy-cib_ebr0.

--- fuzzers/machxo2/035-copy-cib_ebr0/fuzzer.py
+++ fuzzers/machxo2/035-copy-cib_ebr0/fuzzer.py
@@ -1,15 +1,16 @@
 import dbcopy
 import pytrellis
 
-# Based on prior fuzzing that wasn't committed, conjecture that CIB_EBR
-# 0,1,2, and DUMMY have the same layout.
+# Based on prior fuzzing conjecture that CIB_EBR
+# 0,1,2, and DUMMY, and CIB_PIC_B0 and CIB_PIC_B_DUMMY have the same layout.
+
+shared_tiles = ["CIB_EBR1", "CIB_EBR2", "CIB_EBR_DUMMY", "CIB_PIC_B0", "CIB_PIC_B_DUMMY"]
 
 def main():
     pytrellis.load_database("../../../database")
-    dbcopy.dbcopy("MachXO2", "LCMXO2-1200HC", "CIB_EBR0", "CIB_EBR1")
-    dbcopy.dbcopy("MachXO2", "LCMXO2-1200HC", "CIB_EBR0", "CIB_EBR2")
-    dbcopy.dbcopy("MachXO2", "LCMXO2-1200HC", "CIB_EBR0", "CIB_EBR_DUMMY")
 
+    for dest in shared_tiles:
+        dbcopy.dbcopy("MachXO2", "LCMXO2-1200HC", "CIB_EBR0", dest)
 
 if __name__ == "__main__":
     main()
