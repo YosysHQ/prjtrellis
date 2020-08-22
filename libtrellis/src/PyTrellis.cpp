7428bfc0cd3ffab6704bb32a45eb03c2da879ccd
William D. Jones <thor0505@comcast.net>
2020-08-22 02:00:11 -0400

    Allow Chip.global_data as backwards-compatible alias for for Chip.global_data_ecp5.

--- libtrellis/src/PyTrellis.cpp
+++ libtrellis/src/PyTrellis.cpp
@@ -181,6 +181,8 @@ BOOST_PYTHON_MODULE (pytrellis)
             .def_readwrite("tiles", &Chip::tiles)
             .def_readwrite("usercode", &Chip::usercode)
             .def_readwrite("metadata", &Chip::metadata)
+            // Alias for backwards compatibility.
+            .def_readwrite("global_data", &Chip::global_data_ecp5)
             .def_readwrite("global_data_ecp5", &Chip::global_data_ecp5)
             .def_readwrite("global_data_machxo2", &Chip::global_data_machxo2)
             .def(self - self);
