53ccaab635bb997bb19a397670fabd8fa9b3f00c
Miodrag Milanovic <mmicko@gmail.com>
2023-03-24 14:52:48 +0100

    global routing  wip

---
+++ fuzzers/machxo3/022-glb-cib_ebr/6900/fuzzer.py
@@ -0,0 +1,67 @@
+from fuzzconfig import FuzzConfig
+import interconnect
+import pytrellis
+import argparse
+from nets import net_product
+
+def mk_nets(tilepos, glb_ids):
+    ud_nets = []
+    rc_prefix = "R{}C{}_".format(tilepos[0], tilepos[1])
+
+    # Up/Down conns
+    ud_nets.extend(net_product(
+        net_product(["R11C{}_VPTX0{{}}00", "R15C{}_VPTX0{{}}00"], [tilepos[1]]),
+        glb_ids))
+
+    # Phantom DCCs- First fill in "T"/"B", and then global id
+    ud_nets.extend(net_product(
+        net_product([rc_prefix + "CLKI{{}}{}_DCC",
+                     # rc_prefix + "JCE{{}}{}_DCC", # TODO: These nets may not exist?
+                     rc_prefix + "CLKO{{}}{}_DCC"], ("T", "B")),
+        glb_ids))
+
+    return ud_nets
+
+def flatten_nets(tilepos):
+    return [nets for netpair in [(0, 4), (1, 5), (2, 6), (3, 7)] for nets in mk_nets(tilepos, netpair)]
+
+jobs = [
+    (FuzzConfig(job="GLB_UPDOWN26", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C10:CIB_EBR0"]), mk_nets((13, 10), (2, 6))),
+    
+    (FuzzConfig(job="GLB_UPDOWN15", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C13:CIB_EBR0"]), mk_nets((13, 13), (1, 5))),
+
+    (FuzzConfig(job="GLB_UPDOWN04", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C16:CIB_EBR0"]), mk_nets((13, 16), (0, 4))),
+
+    (FuzzConfig(job="GLB_UPDOWN37", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C7:CIB_EBR0"]),  mk_nets((13, 7),  (3, 7))),
+
+    (FuzzConfig(job="CIB_EBR0_END2_DLL45_UPDOWN", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C1:CIB_EBR0_END2_DLL45"]), flatten_nets((13,1))),
+    (FuzzConfig(job="CIB_EBR2_END1_UPDOWN", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R13C41:CIB_EBR2_END1"]), flatten_nets((13,41))),
+
+    (FuzzConfig(job="CIB_EBR0_END2_DLL3_UPDOWN", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R20C1:CIB_EBR0_END2_DLL3"]), flatten_nets((20,1))),
+    (FuzzConfig(job="CIB_EBR2_END1_SP_UPDOWN", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap.ncl",
+                      tiles=["CIB_R20C41:CIB_EBR2_END1_SP"]), flatten_nets((20,41)))
+]
+
+def main(args):
+    pytrellis.load_database("../../../../database")
+
+    for job in [jobs[i] for i in args.ids]:
+        cfg, netnames = job
+        cfg.setup()
+        interconnect.fuzz_interconnect_with_netnames(config=cfg, netnames=netnames,
+                                                     netname_filter_union=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Center Mux Routing Fuzzer.")
+    parser.add_argument(dest="ids", metavar="N", type=int, nargs="*",
+                    default=range(0, len(jobs)), help="Job (indices) to run.")
+    args = parser.parse_args()
+    main(args)
--- fuzzers/machxo3/024-glb-branch/fuzzer.py
+++ fuzzers/machxo3/024-glb-branch/fuzzer.py
@@ -23,6 +23,7 @@ jobs = [
     (FuzzConfig(job="GLB_BRANCH04", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["R5C14:PLC"]), ["R5C15_HPBX0000", "R5C15_HPBX0400"]),
 
+    #4
     # CIB_EBR
     (FuzzConfig(job="CIB_EBR_BRANCH26", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["CIB_R6C4:CIB_EBR0"]), ["R6C5_HPBX0200", "R6C5_HPBX0600"]),
@@ -33,7 +34,8 @@ jobs = [
     (FuzzConfig(job="CIB_EBR_BRANCH37", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["CIB_R6C17:CIB_EBR0"]), ["R6C18_HPBX0300", "R6C18_HPBX0700"]),
 
-    # CIB_PIC_T0.
+    #8
+    # CIB_PIC_T0
     (FuzzConfig(job="CIB_PIC_T0_BRANCH37", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["CIB_R1C9:CIB_PIC_T0"]), ["R1C10_HPBX0300", "R1C10_HPBX0700"]),
     (FuzzConfig(job="CIB_PIC_T0_BRANCH04", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
@@ -43,21 +45,70 @@ jobs = [
     (FuzzConfig(job="CIB_PIC_T0_BRANCH26", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["CIB_R1C12:CIB_PIC_T0"]), ["R1C13_HPBX0200", "R1C13_HPBX0600"]),
 
-    # CIB_EBR0_END0
+    #12
+    # CIB_EBR0_END0 - L - 1300
     (FuzzConfig(job="CIB_EBR0_END0_BRANCH", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["CIB_R6C1:CIB_EBR0_END0"]), ["R6C1_HPBX0100", "R6C2_HPBX0200", "R6C2_HPBX0300",
                                                           "R6C1_HPBX0500", "R6C2_HPBX0600", "R6C2_HPBX0700"]),
+    # CIB_EBR2_END0 - R - 1300
+    (FuzzConfig(job="CIB_EBR2_END0_BRANCH", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
+                      tiles=["CIB_R6C22:CIB_EBR2_END0"]), ["R6C22_HPBX0000", "R6C22_HPBX0100",
+                                                           "R6C22_HPBX0400", "R6C22_HPBX0500"]),
+    # CIB_EBR0_END2_DLL45 - L - 6900
+    (FuzzConfig(job="CIB_EBR0_END2_DLL45_BRANCH", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap_6900.ncl",
+                      tiles=["CIB_R13C1:CIB_EBR0_END2_DLL45"]), ["R13C2_HPBX0000", "R13C2_HPBX0100", "R13C1_HPBX0300",
+                                                                 "R13C2_HPBX0400", "R13C2_HPBX0500", "R13C1_HPBX0700"]),
+    # CIB_EBR2_END1 - R - 2100, 4300 (same as 2100), 6900
+    (FuzzConfig(job="CIB_EBR2_END1_BRANCH", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap_6900.ncl",
+                      tiles=["CIB_R13C41:CIB_EBR2_END1"]), ["R13C41_HPBX0100", "R13C41_HPBX0200",
+                                                            "R13C41_HPBX0500", "R13C41_HPBX0600",]),
+    #16
+    (FuzzConfig(job="CIB_EBR2_END1_2100_BRANCH", family="MachXO3", device="LCMXO3LF-2100C", ncl="tap_2100.ncl",
+                      tiles=["CIB_R8C26:CIB_EBR2_END1"]), ["R8C26_HPBX0000", "R8C26_HPBX0300",
+                                                           "R8C26_HPBX0400", "R8C26_HPBX0700"]),
+    # CIB_EBR0_END2_DLL3 - L - 6900
+    (FuzzConfig(job="CIB_EBR0_END2_DLL3_BRANCH", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap_6900.ncl",
+                      tiles=["CIB_R20C1:CIB_EBR0_END2_DLL3"]), ["R20C2_HPBX0000", "R20C2_HPBX0100", "R20C1_HPBX0300",
+                                                                "R20C2_HPBX0400", "R20C2_HPBX0500", "R20C1_HPBX0700"]),
+
+    # CIB_EBR2_END1_SP - R - 6900, 9400
+    (FuzzConfig(job="CIB_EBR2_END1_SP_BRANCH", family="MachXO3", device="LCMXO3LF-6900C", ncl="tap_6900.ncl",
+                      tiles=["CIB_R20C41:CIB_EBR2_END1_SP"]), ["R20C41_HPBX0100", "R20C41_HPBX0200",
+                                                               "R20C41_HPBX0500", "R20C41_HPBX0600",]),
+    (FuzzConfig(job="CIB_EBR2_END1_SP_9400_BRANCH", family="MachXO3", device="LCMXO3LF-9400C", ncl="tap_9400.ncl",
+                      tiles=["CIB_R8C49:CIB_EBR2_END1_SP"]), ["R8C49_HPBX0000", "R8C49_HPBX0300",
+                                                              "R8C49_HPBX0400", "R8C49_HPBX0700"]),
+    #20
+    # CIB_EBR0_END0_DLL5 - L - 9400
+    (FuzzConfig(job="CIB_EBR0_END0_DLL5_BRANCH", family="MachXO3", device="LCMXO3LF-9400C", ncl="tap_9400.ncl",
+                      tiles=["CIB_R8C1:CIB_EBR0_END0_DLL5"]), ["R8C1_HPBX0100", "R8C2_HPBX0200", "R8C2_HPBX0300",
+                                                               "R8C1_HPBX0500", "R8C2_HPBX0600", "R8C2_HPBX0700"]),
+    # CIB_EBR0_END0_DLL3 - L - 9400
+    (FuzzConfig(job="CIB_EBR0_END0_DLL3_BRANCH", family="MachXO3", device="LCMXO3LF-9400C", ncl="tap_9400.ncl",
+                      tiles=["CIB_R22C1:CIB_EBR0_END0_DLL3"]), ["R22C1_HPBX0100", "R22C2_HPBX0200", "R22C2_HPBX0300",
+                                                                "R22C1_HPBX0500", "R22C2_HPBX0600", "R22C2_HPBX0700"]),
+    # CIB_EBR_DUMMY_END3 - L - 2100
+    (FuzzConfig(job="CIB_EBR_DUMMY_END3_BRANCH", family="MachXO3", device="LCMXO3LF-2100C", ncl="tap_2100.ncl",
+                      tiles=["CIB_R8C1:CIB_EBR_DUMMY_END3"]), ["R8C1_HPBX0000", "R8C2_HPBX0100", "R8C2_HPBX0200",
+                                                               "R8C1_HPBX0400", "R8C2_HPBX0500", "R8C2_HPBX0600"]),
+    # CIB_EBR0_END1 - L - 4300 
+    (FuzzConfig(job="CIB_EBR0_END1_BRANCH", family="MachXO3", device="LCMXO3LF-4300C", ncl="tap_4300.ncl",
+                      tiles=["CIB_R11C1:CIB_EBR0_END1"]), ["R11C2_HPBX0000", "R11C1_HPBX0200", "R11C2_HPBX0300",
+                                                           "R11C2_HPBX0400", "R11C1_HPBX0600", "R11C2_HPBX0700"]),
+    #24
+    # CIB_EBR0_END0_10K - L - 9400
+    (FuzzConfig(job="CIB_EBR0_END0_10K_BRANCH", family="MachXO3", device="LCMXO3LF-9400C", ncl="tap_9400.ncl",
+                      tiles=["CIB_R15C1:CIB_EBR0_END0_10K"]), ["R15C2_HPBX0000", "R15C1_HPBX0100", "R15C2_HPBX0200", "R15C2_HPBX0300",
+                                                               "R15C2_HPBX0400", "R15C1_HPBX0500", "R15C2_HPBX0600", "R15C2_HPBX0700"]),
+    # CIB_EBR2_END1_10K - R - 9400
+    (FuzzConfig(job="CIB_EBR2_END1_10K_BRANCH", family="MachXO3", device="LCMXO3LF-9400C", ncl="tap_9400.ncl",
+                      tiles=["CIB_R15C49:CIB_EBR2_END1_10K"]), ["R15C49_HPBX0000", "R15C49_HPBX0300",
+                                                                "R15C49_HPBX0400", "R15C49_HPBX0700",]),
 
     # PIC_L0
     (FuzzConfig(job="PIC_L0_BRANCH", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["PL5:PIC_L0"]), ["R5C1_HPBX0100", "R5C2_HPBX0200", "R5C2_HPBX0300",
                                               "R5C1_HPBX0500", "R5C2_HPBX0600", "R5C2_HPBX0700"]),
-
-    # CIB_EBR2_END0- This appears to be a noop after other fuzzers run.
-    (FuzzConfig(job="CIB_EBR2_END0_BRANCH", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
-                      tiles=["CIB_R6C22:CIB_EBR2_END0"]), ["R6C22_HPBX0000", "R6C22_HPBX0100",
-                                                          "R6C22_HPBX0400", "R6C22_HPBX0500"]),
-
     # PIC_R0- This also appears to be a noop after other fuzzers run.
     (FuzzConfig(job="PIC_R0_BRANCH", family="MachXO3", device="LCMXO3LF-1300E", ncl="tap.ncl",
                       tiles=["PR5:PIC_R0"]), ["R5C22_HPBX0000", "R5C22_HPBX0100",
