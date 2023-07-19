379d1809b3dfdd11ac74ef4a23ffae69b4917280
Lofty <dan.ravensloft@gmail.com>
2023-07-19 17:50:34 +0100

    machxo3: add modified PicoRV32 to get block RAM timings

--- timing/fuzzers/MachXO3/010-basic-cells/fuzzer.py
+++ timing/fuzzers/MachXO3/010-basic-cells/fuzzer.py
@@ -2,7 +2,7 @@ import cell_fuzzers
 
 
 def main():
-    cell_fuzzers.build_and_add(["../../../resource/picorv32_large.v", "../../../resource/distributed_ram.v"], density="6900", family="MachXO3")
+    cell_fuzzers.build_and_add(["../../../resource/picorv32_large.v", "../../../resource/picorv32_large_blockram.v", "../../../resource/distributed_ram.v"], density="6900", family="MachXO3")
 
 
 if __name__ == "__main__":
