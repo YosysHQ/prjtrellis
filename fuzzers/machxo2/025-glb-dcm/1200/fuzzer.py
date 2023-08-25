baba2b11d9bb7963bd6de8fad6bbb7a378d15d19
Miodrag Milanovic <mmicko@gmail.com>
2023-08-25 12:19:02 +0200

    glb-dcc glb-dcm

--- fuzzers/machxo2/025-glb-dcm/1200/fuzzer.py
+++ fuzzers/machxo2/025-glb-dcm/1200/fuzzer.py
@@ -7,28 +7,27 @@ jobs = [
     (FuzzConfig(job="GLB_DCC", family="MachXO2", device="LCMXO2-1200HC", ncl="empty.ncl",
                           tiles=["CENTER6:CENTER_EBR_CIB", "CENTER_EBR14:CENTER_EBR",
                                  "CENTER9:CENTER8", "CENTER8:CENTER7", "CENTER7:CENTER6",
-                                 "CENTER5:CENTER5", "CENTER4:CENTER4"]), 8)
+                                 "CENTER5:CENTER5", "CENTER4:CENTER4"]))
 ]
 
-
 def main():
-    pytrellis.load_database("../../../database")
+    pytrellis.load_database("../../../../database")
 
     for job in jobs:
-        cfg, N = job
+        cfg = job
         cfg.setup()
         empty_bitfile = cfg.build_design(cfg.ncl, {})
-        cfg.ncl = "dcc.ncl"
-        for i in range(N):
-            loc = "DCC{}".format(i)
-            def get_substs(mode="DCCA"):
+        cfg.ncl = "dcm.ncl"
+        for i in range(6,8):
+            loc = "DCM{}".format(i)
+            def get_substs(mode="DCMA"):
                 if mode == "NONE":
                     comment = "//"
                 else:
                     comment = ""
                 return dict(site=loc, comment=comment)
 
-            nonrouting.fuzz_enum_setting(cfg, "{}.MODE".format(loc), ["NONE", "DCCA"],
+            nonrouting.fuzz_enum_setting(cfg, "{}.MODE".format(loc), ["NONE", "DCMA"],
                                          lambda x: get_substs(mode=x), empty_bitfile, False)
 
 if __name__ == "__main__":
