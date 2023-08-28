7a7d2864a65235a13267f66cdf4eb0e0e36b8457
Miodrag Milanovic <mmicko@gmail.com>
2023-08-28 11:11:29 +0200

    sysconfig

---
+++ fuzzers/machxo2/142-bitargs/fuzzer.py
@@ -0,0 +1,30 @@
+from fuzzconfig import FuzzConfig
+import nonrouting
+import pytrellis
+import fuzzloops
+import interconnect
+import os
+
+cfg = FuzzConfig(job="BITARGS", family="MachXO2", device="LCMXO2-1200HC", ncl="empty.ncl",
+                 tiles=["PT4:CFG0"])
+
+
+def get_substs(config):
+    os.environ["BITARGS"] = " ".join(["-g {}:{}".format(k, v) for k, v in config.items()])
+    return {}
+
+def main():
+    pytrellis.load_database("../../../database")
+    cfg.setup()
+    empty_bitfile = cfg.build_design(cfg.ncl, {})
+    nonrouting.fuzz_enum_setting(cfg, "SYSCONFIG.DONEPHASE", ["T0", "T1", "T2", "T3"],
+                                 lambda x: get_substs(dict(DONEPHASE=x)), empty_bitfile, False)
+    nonrouting.fuzz_enum_setting(cfg, "SYSCONFIG.GOEPHASE", ["T1", "T2", "T3"],
+                                 lambda x: get_substs(dict(GOEPHASE=x)), empty_bitfile, False)
+    nonrouting.fuzz_enum_setting(cfg, "SYSCONFIG.GSRPHASE", ["T1", "T2", "T3"],
+                                 lambda x: get_substs(dict(GSRPHASE=x)), empty_bitfile, False)
+    nonrouting.fuzz_enum_setting(cfg, "SYSCONFIG.GWEPHASE", ["T1", "T2", "T3"],
+                                 lambda x: get_substs(dict(GWEPHASE=x)), empty_bitfile, False)
+
+if __name__ == "__main__":
+    main()
