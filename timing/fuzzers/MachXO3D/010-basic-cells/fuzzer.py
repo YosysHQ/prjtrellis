8b8c361c1615cea488de2b00316ed99e41db8bb9
Miodrag Milanovic <mmicko@gmail.com>
2023-09-21 15:13:55 +0200

    update XO3 and XO3D scripts

--- timing/fuzzers/MachXO3D/010-basic-cells/fuzzer.py
+++ timing/fuzzers/MachXO3D/010-basic-cells/fuzzer.py
@@ -9,7 +9,7 @@ def rewrite_celltype_XO3D(name, type):
     return type.split('/')[-1].split("_")[0]
 
 def main():
-    cell_fuzzers.build_and_add(["../../../resource/picorv32_large.v", "../../../resource/picorv32_large_blockram.v", "../../../resource/distributed_ram.v"], density="9400", family="MachXO3D", inc_cell=include_cell_XO3D, rw_cell_func=rewrite_celltype_XO3D)
+    cell_fuzzers.build_and_add(["../../../resource/picorv32_large.v", "../../../resource/picorv32_large_blockram.v", "../../../resource/distributed_ram.v", "../../../resource/altair.v", "../../../resource/jt49.v", "../../../resource/jt5205.v", "../../../resource/jt7759.v"], density="9400", family="MachXO3D", inc_cell=include_cell_XO3D, rw_cell_func=rewrite_celltype_XO3D)
 
 
 if __name__ == "__main__":
